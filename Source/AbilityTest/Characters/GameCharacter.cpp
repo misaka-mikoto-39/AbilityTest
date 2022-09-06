6253845b9985c3c0cc5e7666a150663d33f7a5d6
Misaka Mikoto <39.misaka.mikoto@gmail.com>
2022-09-07 00:01:45 +0900

    ! rearrange project folder

--- Source/AbilityTest/Characters/GameCharacter.cpp
+++ Source/AbilityTest/Characters/GameCharacter.cpp
@@ -29,7 +29,7 @@ AGameCharacter::AGameCharacter()
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->JumpZVelocity = 500.f;
 	GetCharacterMovement()->AirControl = 0.35f;
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
