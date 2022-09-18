76afc77cb87afadfb1bafbced566a04a0465b15d
Misaka Mikoto <39.misaka.mikoto@gmail.com>
2022-09-18 22:47:09 +0900

    + Dash Ability, UI with cooldown (WIP)

---
+++ Source/AbilityTest/Characters/Abilities/GameplayAbilityBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayAbilityBase.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ABILITYTEST_API UGameplayAbilityBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityBase")
+		UMaterialInstance* UImaterial;
+};
--- Source/AbilityTest/Characters/GameCharacter.cpp
+++ Source/AbilityTest/Characters/GameCharacter.cpp
@@ -34,7 +34,7 @@ AGameCharacter::AGameCharacter()
 	// instead of recompiling to adjust them
 	GetCharacterMovement()->JumpZVelocity = 500.f;
 	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 
